066d6eedce7375a6a49a9380e70b5f59ce2a5d5c
Vincent Incarvite <vincent.incarvite@gmail.com>
2020-09-12 16:43:18 -0400

    Stubbed out some components

---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Content.css'
+
+class Content extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return (<div class=""></div>);
+	}
+}
+
+export default Content
