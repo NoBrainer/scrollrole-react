f3e9f3e6a3191c76add85fffcb5e821a9e83f4da
Vincent Incarvite <vincent.incarvite@gmail.com>
2020-10-08 10:44:17 -0400

    Consolidated yaml API code

--- server/api/yaml.js
+++ server/api/yaml.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const router = express.Router();
-const yaml = require('../common/yaml');
+const yaml = require('js-yaml');
 const {logger} = require('../common/logger');
+
+const router = express.Router();
 
 /* POST JSON string to serialize into YAML */
 router.get('/serialize', (req, res, next) => {
 	//TODO: change from GET to POST
 	//TODO: check for bad requests and such
-	yaml.serialize(req.body)
+	serialize(req.body)
 		.then((response) => {
 			res.status(200).send(response);
 		})
@@ -23,7 +24,7 @@ router.get('/serialize', (req, res, next) => {
 router.get('/deserialize', (req, res, next) => {
 	//TODO: change from GET to POST
 	//TODO: check for bad requests and such
-	yaml.deserialize(req.body)
+	deserialize(req.body)
 		.then((response) => {
 			res.status(200).send(response);
 		})
@@ -34,5 +35,27 @@ router.get('/deserialize', (req, res, next) => {
 			});
 		});
 });
+
+function serialize(json) {
+	return new Promise((resolve, reject) => {
+		try {
+			const text = yaml.safeDump(json);
+			resolve(text);
+		} catch(e) {
+			reject(e);
+		}
+	});
+}
+
+function deserialize(text) {
+	return new Promise((resolve, reject) => {
+		try {
+			const json = yaml.safeLoad(text);
+			resolve(json);
+		} catch(e) {
+			reject(e);
+		}
+	});
+}
 
 module.exports = router
